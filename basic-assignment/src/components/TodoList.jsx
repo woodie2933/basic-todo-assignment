266ba9ffde9310f3808ce22d85c48a7e7cbf2ffd
도니 김 <kdh4847@naver.com>
2025-01-22 16:51:42 +0900

    리스트 배열 불러오기, 호출, css파일 연결

---
+++ basic-assignment/src/components/TodoList.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const todos = [
+  { id: 1, text: "Buy milk" },
+  { id: 2, text: "Clean the house" },
+  { id: 3, text: "Go for a run" },
+  { id: 4, text: "Finish homework" },
+  { id: 5, text: "Call mom" },
+  { id: 6, text: "Buy groceries" },
+  { id: 7, text: "Walk the dog" },
+  { id: 8, text: "Read a book" },
+  { id: 9, text: "Do laundry" },
+  { id: 10, text: "Write code" },
+];
+
+const TodoList = () => {
+  return (
+    <ul>
+      {todos.map((todo) => (
+        <li key={todo.id}>{todo.text}</li>
+      ))}
+    </ul>
+  );
+};
+
+export default TodoList;
